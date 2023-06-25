e3eeda13a209f68ff69db634c9c78bc92d534559
johnrhimawan <johnr.himawan@gmail.com>
2023-06-25 13:09:06 +0800

    Top K Frequent Elements #347

--- Medium/top-k-frequent-elements.py
+++ Medium/top-k-frequent-elements.py
@@ -1,13 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_count = [0] * max(nums)
-        for num in nums:
-            num_count[num] += 1
-        
-        sorted_count = sorted(num_count, reverse=True)
-
-        count = 0
+        num_count = {}
+        num_array = []
         result = []
-        for item in sorted_count:
-            result.append(num_count.index(item))
-
+        for num in nums:
+            num_count[num] = num_count.get(num, 0) + 1
+        for key in num_count:
+            num_array.append(key)
+        num_array.sort(key=lambda x: num_count[x], reverse=True)
+        for i in range(k):
+            result.append(num_array[i])
+        return result
